a3ffaa992bc07978d714107814628bd04c46fe4b
Sergio Soares <acad.sergio@gmail.com>
2017-12-01 14:14:32 -0300

    Implemented newspapper project to use DatabaseTransactions inside Test and practice more about Testing.

---
+++ newspaper/tests/Unit/ArticleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Article;
+
+class ArticleTest extends TestCase
+{
+
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_fetches_trending_articles()
+    {
+        //Given - Arrange
+        factory(\App\Article::class, 2)->create();
+        factory(\App\Article::class)->create(['reads' => 10]);
+        $mostPopular = factory(\App\Article::class)->create(['reads' => 20]);
+
+        //When - Act
+        $articles = Article::trending();
+
+        //Then - Assert
+        $this->assertEquals($mostPopular->id, $articles->first()->id);
+        $this->assertCount(4, $articles);
+
+    }
+}
